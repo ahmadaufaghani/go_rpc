32eaedb39366b7da8e0fb7d3d84194eab40610f9
Ahmad Aufa Ghani <ahmadaufaghani1224@gmail.com>
2026-04-05 11:41:33 +0700

    penambahan komentar penjelasan pada setiap baris

--- client/main.go
+++ client/main.go
@@ -7,19 +7,29 @@ import (
 )
 
 func main() {
+	// Variabel i yang bertipe integer berfungsi sebagai counter pada looping.
+	// Variabel n yang bertipe integer berfungsi untuk menerima inputan angka dari pengguna.
 	var i, n int
+
+	// Array angka[] int berfungsi sebagai variabel yang akan digunakan pada RPC method sebagai argumen berjenis in/out.
 	var angka []int
+
+	// Memanggil RPC melalui protokol HTTP berdasarkan transport layer dan port yang telah ditentukan pada server.
 	client, err := rpc.DialHTTP("tcp", "localhost:4040")
 
+	// Error handling pada RPC.
 	if err != nil {
 		log.Fatal("Connection error: ", err)
 	}
 
+	// Melakukan input angka pada variabel n.
 	fmt.Print("Silahkan masukkan angka untuk batas: ")
 	fmt.Scanf("%d", &n)
 
+	// Memanggil method RpcPrime melalui tipe alias yang telah diregistrasi pada RPC beserta argumen-argumen yang diperlukan.
 	client.Call("API.RpcPrime", n, &angka)
 
+	// Looping untuk mengoutputkan response yang diberikan oleh RPC melalui server.
 	for i = 0; i < len(angka); i++ {
 		fmt.Print(angka[i], " ")
 	}
--- server/main.go
+++ server/main.go
@@ -7,40 +7,64 @@ import (
 	"net/rpc"
 )
 
+// Tipe alias untuk mengubah fungsi menjadi method yang dapat diekspor dan digunakan melalui RPC.
 type API int
 
+// Method RpcPrime yang akan diekspor sehingga dapat dipanggil melalui RPC.
 func (a *API) RpcPrime(n int, reply *[]int) error {
+	// Variabel i dan j yang bertipe integer berfungsi sebagai counter pada looping, serta total untuk menghitung jumlah bilangan yang dapat membagi angka yang sedang dilakukan pengecekan apakah bilangan tersebut prima atau bukan.
 	var i, j, total int
+
+	// Array angka bertipe integer berfungsi untuk menyimpan angka mana saja yang termasuk bilangan prima berdasarkan batas angka maksimal yang diberikan oleh pengguna.
 	var angka []int
+
+	// Looping angka hingga batas angka maksimal yang diberikan oleh pengguna.
 	for i = 0; i < n; i++ {
 		total = 0
+
+		// Looping untuk menghitung berapa banyak jumlah bilangan yang dapat membagi habis angka yang sekarang sedang dilakukan pengecekan (mulai dari angka-angka sebelumnya hingga dibagi oleh angka itu sendiri).
 		for j = 1; j <= i+1; j++ {
 			if (i+1)%j == 0 {
 				total = total + 1
 			}
 		}
+
+		// Melakukan pengecekan pada angka yang sedang diperiksa, jika jumlah bilangan yang dapat membagi habis pada variabel total sama dengan 2, maka angka tersebut akan ditambahkan pada Array var angka []int, karena termasuk bilangan prima.
 		if total == 2 {
 			angka = append(angka, i+1)
 		}
 	}
 
+	// Array var angka[]int yang berisi kumpulan bilangan prima sesuai batas angka yang diberikan oleh pengguna, akan dikembalikan melalui atribut method reply *[]int yang berjenis in/out.
 	*reply = angka
 	return nil
 }
 
 func main() {
+	// Mengalokasi memori untuk tipe alias dan pointer yang baru dialokasi untuk tipe data tersebut disimpan pada variabel api.
 	var api = new(API)
+
+	// RPC melakukan registrasi terhadap variabel tersebut yang berisi alokasi memori dan pointer tipe data alias, sehingga apabila ingin melakukan pemanggilan method dapat dilakukan melalui tipe alias tersebut.
 	err := rpc.Register(api)
+
+	// Error handling pada RPC.
 	if err != nil {
 		log.Fatal("error registering API", err)
 	}
 
+	// Mengatur RPC agar dapat melakukan request dan response pada client melalui protokol HTTP pada server yang sedang dijalankan.
 	rpc.HandleHTTP()
 
+	// Mengatur server agar berjalan pada port 4040 dan menggunakan transport layer TCP yang kemudian disimpan pada variabel listener.
 	listener, err := net.Listen("tcp", ":4040")
 
+	// Log untuk menginformasikan bahwa server untuk RPC telah berjalan pada port 4040.
 	log.Printf("running rpc on port %d", 4040)
+
+	// Menjalankan server yang telah disiapkan berdasarkan pengaturan sebelumnya pada variabel listener menggunakan modul http.
 	err = http.Serve(listener, nil)
+
+	// Error handling pada http.
 	if err != nil {
 		log.Fatal("error running: ", err)
 	}
